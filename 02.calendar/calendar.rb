5912a8f4ca019e880fa2c070dc29b1dba73e1f47
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-02-04 18:38:12 +0900

    変数名を修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,27 +1,27 @@
 require 'optparse'
 require 'date'
 
-@d = Date.today       #今日の日付を取得
+
 
 opt = OptionParser.new
 params = {}
 
-def year_hantei(nen)    #西暦の入力内容を確認して補正する
+def year_hantei(nen,yflag)    #西暦の入力内容を確認して補正する
   unless nen >= 1970 && nen <=2100
-    if @year_nil_flag == 0      #西暦を指定していた(nillでない)場合
+    if yflag == 0      #西暦を指定していた(nillでない)場合
       puts "計算範囲外のため今年の西暦で表示します。"
     end
-    nen = @d.year
+    nen = Date.today.year
   end 
   return nen
 end
 
-def month_hantei(gatu)     #月の入力内容を確認して補正する
+def month_hantei(gatu,mflag)     #月の入力内容を確認して補正する
   unless gatu >=1 && gatu <=12  
-    if @month_nil_flag ==0     #月を指定していた(nillでない)場合
+    if mflag == 0     #月を指定していた(nillでない)場合
       puts '計算範囲外のため今月と同じ"月"で表示します。'
     end
-    gatu = @d.month
+    gatu = Date.today.month
   end
   return gatu
 end
@@ -38,24 +38,24 @@ end
 
 
 #西暦を確定する
-@year_nil_flag = 0
+year_nil_flag = 0
 if params[:y] == nil       #西暦を指定しなかったときフラグ=１
-  @year_nil_flag = 1
+  year_nil_flag = 1
 end
-seireki = year_hantei(params[:y].to_i)  #西暦をseirekiにセット
+seireki = year_hantei(params[:y].to_i,year_nil_flag)  #西暦をseirekiにセット
 
 #月を確定する
-@month_nil_flag = 0        
+month_nil_flag = 0        
 if params[:m] == nil       #月を指定しなかった時フラグ=１
-  @month_nil_flag = 1
+  month_nil_flag = 1
 end
-tuki = month_hantei(params[:m].to_i)    #月をtukiにセット
+tuki = month_hantei(params[:m].to_i,month_nil_flag)    #月をtukiにセット
 
 
 #反転表示が必要な場合(今日の日付が含まれている)　フラグ=1
-@today_hanntenn_flag = 0
-if seireki == @d.year && tuki == @d.month
-  @today_hanntenn_flag = 1
+today_hanntenn_flag = 0
+if seireki == Date.today.year && tuki == Date.today.month
+  today_hanntenn_flag = 1
 end
 
 #カレンダーの１日の曜日を取得する
@@ -82,13 +82,13 @@ end
     print("\n")
   end
   if i >= 1 && i <= 9
-    if @today_hanntenn_flag == 1 && i == @d.mday
+    if today_hanntenn_flag == 1 && i == Date.today.mday
       print("\e[7m"+" #{i}"+"\e[0m"+" ") 
     else
       print(" #{i} ")
     end
   else
-    if @today_hanntenn_flag == 1 && i == @d.mday
+    if today_hanntenn_flag == 1 && i == Date.today.mday
       print("\e[7m"+"#{i}"+"\e[0m"+" ")
     else
       print("#{i} ")
@@ -96,3 +96,4 @@ end
   end
   x += 1
 end
+

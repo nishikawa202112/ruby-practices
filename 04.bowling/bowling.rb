a756f520ccbabef70b97e1a6c0bc936c5983e240
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-02-13 16:12:56 +0900

    ボウリングのスコア計算プログラムを修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -14,27 +14,22 @@ score.each do |s|
 end
 
 # フレームごとに分ける
-frames = []
-scores.each_slice(2) do |s|
-  frames.push(s)
-end
+frames = scores.each_slice(2).to_a
 
 # 点数を計算する
-point = 0
-(0..8).each do |i|
-  if frames[i][0] == 10 # ストライク
-    point += 10 + frames[i + 1].sum
-    point += frames[i + 2][0] if frames[i + 1][0] == 10
+point = (0..8).sum do |i|
+  if frames[i][0] == 10 && frames[i + 1][0] == 10 # 次のフレームもストライクの時
+    20 + frames[i + 2][0]
+  elsif frames[i][0] == 10 # ストライク
+    10 + frames[i + 1].sum
   elsif frames[i].sum == 10 # スペア
-    point += 10 + frames[i + 1][0]
+    10 + frames[i + 1][0]
   else
-    point += frames[i].sum
+    frames[i].sum
   end
 end
 
-(9..frames.size - 1).each do |i|
-  point += frames[i].sum
-end
+point += (9..frames.size - 1).sum { |i| frames[i].sum }
 
 # 点数を表示する
 puts point

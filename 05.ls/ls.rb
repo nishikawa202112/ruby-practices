6bffdedbfec5bbb3d76580edf175a8bd783893a0
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-03-12 15:33:52 +0900

    lsコマンドを作る１を再提出(3)

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,46 +5,35 @@ require 'io/console'
 def main
   _, columns = $stdout.winsize
   files = Dir.glob('*')
-  max_name_length = create_max_name_length(files)
-  column_count, line_count = seek_column_and_line(columns, max_name_length, files)
-  names = create_names(files, line_count, column_count)
-  print_names(names, max_name_length, column_count, line_count)
+  column_width = calc_column_width(files)
+  column_count, line_count = calc_col_and_row_count(columns, column_width, files)
+  names = create_names(files, line_count)
+  print_names(names, column_width, column_count, line_count)
 end
 
-def create_max_name_length(files)
+def calc_column_width(files)
   max_name_length = files.max_by(&:length).length
   column_space = 8 - max_name_length % 8
   max_name_length + column_space
 end
 
-def seek_column_and_line(columns, max_name_length, files)
-  column_count = columns / max_name_length
+def calc_col_and_row_count(columns, column_width, files)
+  column_count = columns / column_width
   line_count = (files.size.to_f / column_count).ceil
   [column_count, line_count]
 end
 
-def create_names(files, line_count, column_count)
+def create_names(files, line_count)
   names = []
-  count = 0
-  column_count.times do
-    names_array = []
-    line_count.times do
-      if count == files.size
-        names_array << ' '
-      else
-        names_array << files[count]
-        count += 1
-      end
-    end
-    names << names_array
-  end
+  names = files.each_slice(line_count).to_a
+  (line_count - names.last.size).times { names.last.push(nil) }
   names.transpose
 end
 
-def print_names(names, max_name_length, column_count, line_count)
-  (0..line_count - 1).each do |i|
-    (0..column_count - 1).each do |n|
-      print names[i][n].ljust(max_name_length) unless names[i][n] == ' '
+def print_names(names, column_width, column_count, line_count)
+  line_count.times do |i|
+    column_count.times do |n|
+      print names[i][n].ljust(column_width) unless names[i][n].nil?
     end
     print("\n")
   end

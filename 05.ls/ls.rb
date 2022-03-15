5292275a9bb0c5277203e7e8ec5afff8b8550beb
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-03-15 15:07:14 +0900

    変数名の変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,12 +3,13 @@
 require 'io/console'
 
 TAB_SPACE = 8
+
 def main
   _, columns = $stdout.winsize
   files = Dir.glob('*')
   column_width = calc_column_width(files)
-  line_count = calc_row_count(columns, column_width, files)
-  names = create_names(files, line_count)
+  row_count = calc_row_count(columns, column_width, files)
+  names = create_names(files, row_count)
   print_names(names, column_width)
 end
 
@@ -23,9 +24,9 @@ def calc_row_count(columns, column_width, files)
   (files.size.to_f / column_count).ceil
 end
 
-def create_names(files, line_count)
-  names = files.each_slice(line_count).to_a
-  (line_count - names.last.size).times { names.last.push(nil) }
+def create_names(files, row_count)
+  names = files.each_slice(row_count).to_a
+  (row_count - names.last.size).times { names.last.push(nil) }
   names.transpose
 end
 
@@ -34,7 +35,7 @@ def print_names(names, column_width)
     line.each do |name|
       print name.ljust(column_width) unless name.nil?
     end
-    print("\n")
+    print "\n"
   end
 end
 

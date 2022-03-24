451152f5a4fb3226e7f7b501f37067ce096fc48b
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-03-24 17:52:33 +0900

    lsコマンドを作る３を作成

---
+++ 05.ls/ls.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'io/console'
+require 'optparse'
+
+TAB_SPACE = 8
+
+def main
+  _, columns = $stdout.winsize
+  files = find_files
+  column_width = calc_column_width(files)
+  row_count = calc_row_count(columns, column_width, files)
+  file_name_lines = create_file_name_lines(files, row_count)
+  print_file_name_lines(file_name_lines, column_width)
+end
+
+def find_files
+  opt = OptionParser.new
+  opt.on('-r')
+  params = {}
+  opt.parse!(ARGV, into: params)
+  if params[:r]
+    Dir.glob('*').reverse
+  else
+    Dir.glob('*')
+  end
+end
+
+def calc_column_width(files)
+  max_name_length = files.max_by(&:length).length
+  column_space = TAB_SPACE - max_name_length % TAB_SPACE
+  max_name_length + column_space
+end
+
+def calc_row_count(columns, column_width, files)
+  column_count = columns / column_width
+  (files.size.to_f / column_count).ceil
+end
+
+def create_file_name_lines(files, row_count)
+  file_name_lines = files.each_slice(row_count).to_a
+  (row_count - file_name_lines.last.size).times { file_name_lines.last.push(nil) }
+  file_name_lines.transpose
+end
+
+def print_file_name_lines(file_name_lines, column_width)
+  file_name_lines.each do |line|
+    line.each do |name|
+      print name.ljust(column_width) unless name.nil?
+    end
+    print "\n"
+  end
+end
+
+main

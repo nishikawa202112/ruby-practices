776d78686105dc51b861920c3dbed3fbf0dfed45
nishikawa <nishiyumn@jcom.zaq.ne.jp>
2022-09-15 22:06:23 +0900

    主にframeクラスを修正しました

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -3,6 +3,8 @@
 class Frame
   attr_reader :shots
 
+  protected :shots
+
   def initialize(shots)
     @shots = shots
   end
@@ -12,7 +14,7 @@ class Frame
   end
 
   def strike?
-    shots[0].strike?
+    shots[0].strike? && shots[1].nil?
   end
 
   def spare?
@@ -22,7 +24,7 @@ class Frame
   def calc_score_with_bonus(next_frame, after_next_frame)
     score +
       if strike?
-        next_frame.strike? && !after_next_frame.nil? ? (next_frame.score + after_next_frame.shots[0].score) : next_frame.shots[0..1].sum(&:score)
+        next_frame.strike? ? (next_frame.score + after_next_frame.shots[0].score) : next_frame.shots[0..1].sum(&:score)
       elsif spare?
         next_frame.shots[0].score
       else
--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 class Shot
-  attr_reader :mark
-
   def initialize(mark)
     @mark = mark
   end
 
   def score
-    strike? ? 10 : mark.to_i
+    strike? ? 10 : @mark.to_i
   end
 
   def strike?
-    mark == 'X'
+    @mark == 'X'
   end
 end
